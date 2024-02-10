733d370fb1f48692d7f7e43145bd948648910f51
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2024-02-10 19:48:06 +0700

    test: sanity

--- test/ERC404.t.sol
+++ test/ERC404.t.sol
@@ -7,11 +7,23 @@ import "../src/contracts/examples/ExampleERC404.sol";
 contract Erc404Test is Test {
     ExampleERC404 public pandora;
 
+    string name_ = "Example";
+    string symbol_ = "EXM";
+    uint8 decimals_ = 18;
+    uint256 maxTotalSupplyNft_ = 100;
+
+    address initialOwner_ = address(0x1);
+    address initialMintRecipient_ = address(0x2);
+
     function setUp() public {
-        pandora = new ExampleERC404("Pandora", "PANDORA", 18, 10000, msg.sender, msg.sender);
+        pandora = new ExampleERC404(name_, symbol_, decimals_, maxTotalSupplyNft_, initialOwner_, initialMintRecipient_);
     }
 
     function test_sanity() public {
-        assertEq(pandora.name(), "Pandora");
+        assertEq(pandora.name(), name_);
+        assertEq(pandora.symbol(), symbol_);
+        assertEq(pandora.decimals(), decimals_);
+        assertEq(pandora.owner(), initialOwner_);
+        assertEq(pandora.balanceOf(initialMintRecipient_), maxTotalSupplyNft_ * 10 ** decimals_);
     }
 }
